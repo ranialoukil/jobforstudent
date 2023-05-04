006367aadc6b5009269b3af6899599ce131b5eb3
ranialoukil <132591920+ranialoukil@users.noreply.github.com>
2023-05-04 17:16:38 +0200

    Add files via upload

---
+++ Header.php
@@ -0,0 +1,18 @@
+    <!-- Header -->
+    <div id="header">
+
+        <!-- Logotyp -->
+      
+        <hr class="noscreen" />          
+
+        <!-- Quick links -->
+        <div class="noscreen noprint">
+            <p><em>Quick links: <a href="#content">content</a>, <a href="#tabs">navigation</a>, <a href="#search">search</a>.</em></p>
+            <hr />
+        </div>
+
+        <div class="titrera">
+  <h1>JOB FOR STUDENT</h1></div>
+
+
+    </div> <!-- /header -->
---
+++ footer.php
@@ -0,0 +1,8 @@
+   <!-- Footer -->
+    <div id="footer">
+        <div id="top" class="noprint"><p><span class="noscreen">Back on top</span> <a href="#header" title="Back on top ^">^<span></span></a></p></div>
+        <hr class="noscreen" />
+        
+        <p class="style1" id="createdby">created by Our Team</p>
+        <p>SARRA DARRAJI AND RANIA LOUKIL</p>
+</div> <!-- /footer -->
